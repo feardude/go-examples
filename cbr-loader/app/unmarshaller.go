b616966063bf6aeaade3b8e546a718704396f0e7
Max Samoylov <feardude@gmail.com>
2019-03-19 13:19:56 +0300

    Remove Currencies parsing from unmarshaller

--- cbr-loader/app/unmarshaller.go
+++ cbr-loader/app/unmarshaller.go
@@ -3,16 +3,15 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"strings"
 	"time"
 )
 
 // Currency represents currency structure
 type Currency struct {
-	CodeCbr string `xml:"Vcode"`
-	CodeEng string `xml:"VcharCode"`
-	NameRus string `xml:"Vname"`
-	NameEng string `xml:"VEngname"`
+	CodeCbr string
+	CodeEng string
+	NameRus string
+	NameEng string
 }
 
 // ToString returns formatted Currency
@@ -45,32 +44,6 @@ func (fxRate FxRate) ToString(code string) string {
 		fxRate.Date.Format(time.RFC3339),
 		code,
 		fxRate.Value)
-}
-
-// Currencies parses input XML to []Currency and returns result
-func Currencies(input *[]byte) []Currency {
-	var currencies struct {
-		CbrCurrencies []Currency `xml:"Body>EnumValutesXMLResponse>EnumValutesXMLResult>ValuteData>EnumValutes"`
-	}
-	err := xml.Unmarshal(*input, &currencies)
-	if err == nil {
-		return processed(currencies.CbrCurrencies)
-	}
-	panic(err)
-}
-
-func processed(cbrCurrencies []Currency) []Currency {
-	processed := make([]Currency, 0)
-	for _, c := range cbrCurrencies {
-		if c.CodeEng != "" {
-			c.CodeCbr = strings.TrimSpace(c.CodeCbr)
-			c.CodeEng = strings.TrimSpace(c.CodeEng)
-			c.NameEng = strings.TrimSpace(c.NameEng)
-			c.NameRus = strings.TrimSpace(c.NameRus)
-			processed = append(processed, c)
-		}
-	}
-	return processed
 }
 
 // FxRates parses input XML to []FxRate and returns result
