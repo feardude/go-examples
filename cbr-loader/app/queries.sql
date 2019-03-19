9310b19acff007f008d61c1d53fd09c134b45170
Max Samoylov <feardude@gmail.com>
2019-03-19 13:09:04 +0300

    Fix FX rate insertion with conflict

--- cbr-loader/app/queries.sql
+++ cbr-loader/app/queries.sql
@@ -20,7 +20,10 @@ on conflict do nothing;
 
 -- name: insert-fx_rate
 insert into fx_rates values ($1, $2, $3)
-on conflict do nothing;
+on conflict (code_cbr, date_time)
+do update set value = $3
+    where fx_rates.code_cbr = $1
+        and fx_rates.date_time = $2;
 
 -- name: select-last-date
 select max(date_time)
