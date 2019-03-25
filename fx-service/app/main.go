5517c39c4b3fcc81761811a00a2806c5513f02b6
Max Samoylov <feardude@gmail.com>
2019-03-25 15:24:00 +0300

    Add stubs for REST server

--- fx-service/app/main.go
+++ fx-service/app/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	log.Println("Started FX service")
@@ -8,11 +13,33 @@ func main() {
 
 	defer ShutdownDB()
 
+	router := getRouter()
+	log.Fatal(http.ListenAndServe(":8080", router))
+
 	log.Println("Finished FX service")
+}
+
+func getCurrencies(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getRate(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/currencies", getCurrencies).Methods("GET")
+	router.HandleFunc("/currencies/{code}", getRate).Methods("GET")
+	return router
+}
+
+func setContentType(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
 }
